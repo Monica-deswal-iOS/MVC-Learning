ed6441d654504dac9b736df87b8d8f311cea20a9
monica deswal <mdeswal@falabella.cl>
2020-08-25 17:27:49 +0530

    SAMPLE PROJECT CREATION

---
+++ MVC_SAMPLE/MVC_SAMPLE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC_SAMPLE
+//
+//  Created by Monica Deswal on 25/08/20.
+//  Copyright © 2020 Monica Deswal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // CREATE THE OBJECT
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
